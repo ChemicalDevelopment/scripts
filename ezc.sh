b05c50a1a344714a768dc1fc6327d49f4f228efe
Cade Brown <brown.cade@gmail.com>
2016-12-25 23:16:25 -0500

    updated

--- ezc.sh
+++ ezc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /tmp/ && curl https://github.com/ChemicalDevelopment/ezc/archive/master.zip -L > ezc.zip && unzip ezc.zip && cd ezc-master && sudo make global
+cd /tmp/ && curl https://github.com/ChemicalDevelopment/ezc/archive/master.zip -L > ezc.zip && unzip -o ezc.zip && cd ezc-master && sudo make global
