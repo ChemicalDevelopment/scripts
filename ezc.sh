e51d9fa185dd12682eda1fae886eebb488f88e42
Cade Brown <brown.cade@gmail.com>
2017-01-05 15:50:05 -0500

    Added fix for archive not found

--- ezc.sh
+++ ezc.sh
@@ -18,10 +18,10 @@ echo "Installing for $TYPE system"
 if [[ "$TYPE" == "build" ]]; then
 	curl chemdev.space/build-ezc.sh -L | bash
 else
-	ARHIVE="https://github.com/ChemicalDevelopment/ezc/releases/download/$RELEASE/$TYPE.tar.xz"
-	echo "Using $ARHIVE"
+	ARCHIVE="https://github.com/ChemicalDevelopment/ezc/releases/download/$RELEASE/$TYPE.tar.xz"
+	echo "Using $ARCHIVE"
 	cd ./
-	curl $ARHIVE -L > ezc.tar.xz
+	curl $ARCHIVE -L > ezc.tar.xz || { echo "Building EZC from source, because I couldn't find an archive for your OS ($TYPE)"; curl chemdev.space/build-ezc.sh -L | bash; exit 0; }
 	tar xfv ezc.tar.xz
 	rm ezc.tar.xz
 fi
