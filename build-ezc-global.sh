09f1b47454f0353fd0e1921280c1322c077803af
Cade Brown <brown.cade@gmail.com>
2017-01-31 23:24:22 -0500

    SH

--- build-ezc-global.sh
+++ build-ezc-global.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/bin/sh
 cd /tmp/ && curl https://github.com/ChemicalDevelopment/ezc/archive/master.zip -L > ezc.zip && unzip -o ezc.zip && cd ezc-master && sudo make global
--- hello.sh
+++ hello.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/bin/sh
 printf "ChemicalDevelopment scripts. Hello!"
