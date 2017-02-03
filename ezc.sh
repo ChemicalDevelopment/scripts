86f21d6a78a1fee9e7c3ee5f971a71a45e06b157
Cade Brown <brown.cade@gmail.com>
2017-02-03 15:33:01 -0500

    Updated EZC install script

--- ezc.sh
+++ ezc.sh
@@ -1,27 +1,37 @@
 #!/bin/sh
-#RELEASE="1.2.3"
-RELEASE="master"
+#RELEASE="master"
 
-UNAMESTR=$(uname)
+if [ "$RELEASE" = "" ]; then
+	RELEASE="1.2.3"
+fi
 
-echo $UNAMESTR
+if [ "$RELEASE" = "dev" ]; then
+	RELEASE="master"
+fi
 
-case "$UNAMESTR" in
-	"Linux") TYPE="linux";;
-	"darwin"*) TYPE="mac";;
-	*"BSD") TYPE="bsd";;
-	*) TYPE="build";;
-esac
+if [ "$PROFILE" = "" ]; then
+	PROFILE=~/.bashrc
+fi
+if [ "$LOCATION" = "" ]; then
+	LOCATION=~/.ezc/
+fi
 
-TYPE="build"
+if [ "$TYPE" = "" ]; then
+	UNAMESTR=$(uname)
+
+	echo $UNAMESTR
+
+	case "$UNAMESTR" in
+		"Linux") TYPE="linux";;
+		"darwin"*) TYPE="mac";;
+		*"BSD") TYPE="bsd";;
+		*) TYPE="build";;
+	esac
+fi
 
 echo "Installing for $TYPE system"
 
-#USE_GIT="true"
-
-LOCATION=~/.ezc/
 BUILD_DIR=$LOCATION/REPO
-PROFILE=~/.bashrc
 PROFILE_RELOAD="export PATH=\$PATH:${LOCATION}"
 mkdir -p ${LOCATION}
 cd ${LOCATION}
@@ -31,15 +41,18 @@ echo "${PROFILE_RELOAD}" >> ${PROFILE}
 if [ "$TYPE" = "build" ]; then
 	mkdir -p $BUILD_DIR
 	cd $BUILD_DIR
-	if [ "$USE_GIT" = "true" ]; then 
-		git clone https://github.com/ChemicalDevelopment/ezc.git
-		cd ezc
-	else
+	if [ "$USE_GIT" = "" ]; then 
 		ARCHIVE="https://github.com/ChemicalDevelopment/ezc/archive/$RELEASE.tar.gz"
 		echo "Using $ARCHIVE"
 			curl $ARCHIVE -L > ezc.tar.gz
-		tar -xvzf ezc.tar.gz
+		tar -xzf ezc.tar.gz
 		cd ezc-*
+	else
+		git clone https://github.com/ChemicalDevelopment/ezc.git
+		cd ezc
+		if [ "$RELEASE" != "master" ]; then
+			git checkout tags/$RELEASE
+		fi
 	fi
 
 	make DIR=$LOCATION
@@ -52,7 +65,7 @@ else
 	echo "Using $ARCHIVE"
 	
 	curl $ARCHIVE -L > ezc.tar.xz
-	tar -xfv ezc.tar.xz
+	tar -xf ezc.tar.xz
 
 	rm ezc.tar.xz
 fi
