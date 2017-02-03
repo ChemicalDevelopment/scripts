0d2e8d0dc8c9248614b6b548e99bcee62c8daaba
Cade Brown <brown.cade@gmail.com>
2017-02-03 10:20:38 -0500

    Updated EZC install script

--- ezc.sh
+++ ezc.sh
@@ -17,13 +17,30 @@ fi
 
 echo "Installing for $TYPE system"
 
-if [ "$TYPE" = "build" ]; then
-	curl chemdev.space/build-ezc.sh -L | bash
-else
+if [ "$TYPE" != "build" ]; then
 	ARCHIVE="https://github.com/ChemicalDevelopment/ezc/releases/download/$RELEASE/ezc-$TYPE.tar.xz"
 	echo "Using $ARCHIVE"
 	cd ./
-	curl $ARCHIVE -L > ezc.tar.xz || { echo "Building EZC from source, because I couldn't find an archive for your OS ($TYPE)"; curl chemdev.space/build-ezc.sh -L | bash; exit 0; }
-	tar xfv ezc.tar.xz
-	rm ezc.tar.xz
+	{
+		LOCATION=~/.ezc/
+		PROFILE=~/.bash_profile
+		PROFILE_RELOAD=export PATH=$${PATH}:${LOCATION}
+		mkdir -p ${LOCATION}
+		cd ${LOCATION}
+
+		curl $ARCHIVE -L > ezc.tar.xz
+		tar xfv ezc.tar.xz
+
+		echo "${PROFILE_RELOAD}" >> ${PROFILE}
+		${PROFILE_RELOAD}
+		rm ezc.tar.xz
+		exit 0;
+	} || {
+		 echo "Building EZC from source, because I couldn't find an archive for your OS ($TYPE)"
+		 TYPE="build"; 
+	}
+fi
+
+if [ "$TYPE" = "build" ]; then
+	curl chemdev.space/build-ezc.sh -L | bash
 fi
