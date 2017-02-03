bba0b05efe97a385d237de2510ddb5fc3c7ecfa6
Cade Brown <brown.cade@gmail.com>
2017-02-03 12:07:39 -0500

    updated install

--- ezc.sh
+++ ezc.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-RELEASE="1.2.3"
+#RELEASE="1.2.3"
+RELEASE="master"
 
 UNAMESTR=$(uname)
 
@@ -12,24 +13,39 @@ case "$UNAMESTR" in
 	*) TYPE="build";;
 esac
 
+TYPE="build"
+
 echo "Installing for $TYPE system"
+LOCATION=~/.ezc/
+BUILD_DIR=$LOCATION/REPO
+PROFILE=~/.bashrc
+PROFILE_RELOAD="export PATH=\$PATH:${LOCATION}"
+mkdir -p ${LOCATION}
+cd ${LOCATION}
+
+echo "${PROFILE_RELOAD}" >> ${PROFILE}
 
 if [ "$TYPE" = "build" ]; then
-	curl chemdev.space/build-ezc.sh -L | bash
+	ARCHIVE="https://github.com/ChemicalDevelopment/ezc/archive/$RELEASE.tar.gz"
+	echo "Using $ARCHIVE"
+	mkdir -p $BUILD_DIR
+	pushd $BUILD_DIR
+	curl $ARCHIVE -L > ezc.tar.gz
+	tar -xvzf ezc.tar.gz
+
+	cd ezc-*
+
+	make DIR=$LOCATION
+	rm ezc.tar.gz
+
+	popd
+	rm -rf $BUILD_DIR
 else
 	ARCHIVE="https://github.com/ChemicalDevelopment/ezc/releases/download/$RELEASE/ezc-$TYPE.tar.xz"
 	echo "Using $ARCHIVE"
-	LOCATION=~/.ezc/
-	PROFILE=~/.bashrc
-	PROFILE_RELOAD="export PATH=\$PATH:${LOCATION}"
-	mkdir -p ${LOCATION}
-	cd ${LOCATION}
-
+	
 	curl $ARCHIVE -L > ezc.tar.xz
-	tar xfv ezc.tar.xz
+	tar -xfv ezc.tar.xz
 
-	echo "${PROFILE_RELOAD}" >> ${PROFILE}
 	rm ezc.tar.xz
-	exit 0;
-
 fi
